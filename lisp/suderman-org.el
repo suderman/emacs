88f2c44f6d09ee8dc51661f52a7644fa76b035f4
Jon Suderman <jon@suderman.net>
2026-08-27 15:05:12 -0600

    fix(org): route leader commands by buffer context

--- lisp/suderman-org.el
+++ lisp/suderman-org.el
@@ -9,6 +9,14 @@
 
 (defvar org-done-keywords)
 
+(declare-function consult-org-agenda "consult-org" (&optional match))
+(declare-function consult-org-heading "consult-org" (&optional match scope))
+(declare-function org-agenda-deadline "org-agenda" (arg &optional time))
+(declare-function org-agenda-refile "org-agenda" (&optional goto rfloc no-update))
+(declare-function org-agenda-schedule "org-agenda" (arg &optional time))
+(declare-function org-agenda-todo "org-agenda" (&optional arg))
+(declare-function org-agenda-write "org-agenda" (file &optional open nosettings _))
+(declare-function org-todo-list "org-agenda" (&optional arg))
 (declare-function org-get-todo-state "org" ())
 (declare-function org-todo "org" (&optional arg))
 (declare-function org-mouse-todo-menu "org-mouse" (state))
@@ -51,6 +59,67 @@
    t))
 
 (add-hook 'org-mode-hook #'suderman/org-enable-mouse-todo-cycling)
+
+(defun suderman/org--call-contextually
+    (org-command &optional agenda-command fallback-command)
+  "Call the command appropriate for the current Org context."
+  (cond
+   ((derived-mode-p 'org-agenda-mode)
+    (if agenda-command
+        (call-interactively agenda-command)
+      (user-error "This command requires an Org buffer")))
+   ((derived-mode-p 'org-mode)
+    (call-interactively org-command))
+   (fallback-command
+    (call-interactively fallback-command))
+   (t
+    (user-error "This command requires an Org buffer"))))
+
+(defun suderman/org-deadline ()
+  "Set an Org item's deadline, or open the TODO list."
+  (interactive)
+  (suderman/org--call-contextually
+   #'org-deadline #'org-agenda-deadline #'org-todo-list))
+
+(defun suderman/org-export ()
+  "Export the current Org buffer or Agenda view."
+  (interactive)
+  (suderman/org--call-contextually
+   #'org-export-dispatch #'org-agenda-write))
+
+(defun suderman/org-heading ()
+  "Jump to an Org heading in the current buffer or agenda files."
+  (interactive)
+  (suderman/org--call-contextually
+   #'consult-org-heading #'consult-org-agenda #'consult-org-agenda))
+
+(defun suderman/org-insert-link ()
+  "Insert a link in an Org buffer."
+  (interactive)
+  (suderman/org--call-contextually #'org-insert-link))
+
+(defun suderman/org-refile ()
+  "Refile an Org item, or open the TODO list."
+  (interactive)
+  (suderman/org--call-contextually
+   #'org-refile #'org-agenda-refile #'org-todo-list))
+
+(defun suderman/org-schedule ()
+  "Schedule an Org item, or open the TODO list."
+  (interactive)
+  (suderman/org--call-contextually
+   #'org-schedule #'org-agenda-schedule #'org-todo-list))
+
+(defun suderman/org-todo ()
+  "Change an Org item's TODO state, or open the TODO list."
+  (interactive)
+  (suderman/org--call-contextually
+   #'org-todo #'org-agenda-todo #'org-todo-list))
+
+(defun suderman/org-toggle-checkbox ()
+  "Toggle a checkbox in an Org buffer."
+  (interactive)
+  (suderman/org--call-contextually #'org-toggle-checkbox))
 
 (defun suderman/org-mouse-todo-menu-with-clear (original state)
   "Add a menu item that clears the current TODO state."
